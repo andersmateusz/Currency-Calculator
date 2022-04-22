21fcb042d2b5edca4fd2cd47c935e6a9d4272411
wazka83 <103202246+wazka83@users.noreply.github.com>
2022-04-22 12:53:18 +0200

    added readme, deleted api key form on page

---
+++ README.txt
@@ -0,0 +1,10 @@
+This is The Currency Calculator web app.
+This app uses exchange rata API. 
+If you want to use this app get your free API key from https://exchangeratesapi.io after registering. 
+To use your API key insert it to:
+/src/Controller/IndexController.php line 20
+/src/Entity/Form/CheckGrowthType.php  line 17
+/src/Entity/Form/ConverterType.php line 15
+as argument for CurrencyManager(_YOUR_API_KEY_HERE).
+By default there is already API key, but it might be not up to date when you are reading this.
+
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\ApiKeyType;
+//use App\Form\ApiKeyType;
 use App\Form\ConverterType;
 use App\Form\CheckGrowthType;
 use App\Entity\CurrencyManager;
@@ -17,13 +17,15 @@ class IndexController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(Request $request): Response
     {
-        $manager=new CurrencyManager('1535a8ffed9a203d2b92a107a95d4ffa');
-        $convertResult=['code1'=>null, 'code2'=>null,'date'=>null, 'result'=>null, 'value'=>null];
-         $equal=null;
-    
-        
-       
-        
+        $manager=new CurrencyManager('1535a8ffed9a203d2b92a107a95d4ffa'); //Insert your API key here
+        //And also in CheckGrowthType.php and ConverterType.php
+        $convertResult=[
+            'code1'=>null, 
+            'code2'=>null,
+            'date'=>null, 
+            'result'=>null, 
+            'value'=>null];
+        $equal=null;
         $checkResult=[
         'date1'=>null,
         'date2'=>null, 
@@ -34,47 +36,45 @@ class IndexController extends AbstractController
         'percent'=>null,
         'isGrowing'=>null
         ];
-        
-        $apiForm=$this->createForm(ApiKeyType::class, null, [
-                'method'=>'POST',
-        ]);
-        //handleRequest only processes data when this is POST method
+        /*
+        // Insert your API key on the page. Not working.
+        $apiForm=$this->createForm(ApiKeyType::class);
         $apiForm->handleRequest($request);
-        
-        if($apiForm->isSubmitted()&&$apiForm->isValid()){
-           dump($apiForm->getData());
+        if($apiForm->isSubmitted()&&$apiForm->isValid())
+        {
            $apiKey=($apiForm->getData())['apiKey'];
-           dump($apiKey);
         };
-        
-      
-        $converterForm=$this->createForm(ConverterType::class, null, [
-        ]);
+        */
+        /////////////////////////////////////////////////////////////////////////////////////////
+        //Converter Form
+        $converterForm=$this->createForm(ConverterType::class);
         $converterForm->handleRequest($request);
-        
         if($converterForm->isSubmitted() && $converterForm->isValid()){
             $converterData=($converterForm->getData());
             $manager->UpdateLatest();
             $convertResult=$manager->Convert($converterData['value'], $converterData['first_currency'], $converterData['second_currency']);
             $equal="=";
          };
-         
-        $checkerForm=$this->createForm(CheckGrowthType::class, null, [
-        ]);
-        
+        /////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////////
+        //Check Currency Rate Growth Form
+        $checkerForm=$this->createForm(CheckGrowthType::class);
         $checkerForm->handleRequest($request);
-        
         if($checkerForm->isSubmitted() && $checkerForm->isValid()){
             $checkerData=($checkerForm->getData());
             dump($convertResult);
-            $checkResult=$manager->CompareCurrency($checkerData['firstDate'],$checkerData['secondDate'], $checkerData['first_currency'], $checkerData['second_currency']);
+            $checkResult=$manager->CompareCurrency(
+                $checkerData['firstDate'],
+                $checkerData['secondDate'], 
+                $checkerData['first_currency'], 
+                $checkerData['second_currency'
+            ]);
          };
-         dump($checkResult);
-        dump($convertResult);
+        /////////////////////////////////////////////////////////////////////////////////////////
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
             'title' => 'Currency Calculator',
-            'ApiKeyType' => $apiForm->createView(),
+            //'ApiKeyType' => $apiForm->createView(),
             'ConverterType'=>$converterForm->createView(),
             'CheckGrowthType' => $checkerForm->createView(),
             'latestCode1' =>  $convertResult['code1'],
